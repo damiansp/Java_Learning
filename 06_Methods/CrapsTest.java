6a217baca74cc299921d8010efee3f6eb848872c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-05-27 18:29:30 -0400

    Added methods examples to 06/

---
+++ 06_Methods/CrapsTest.java
@@ -0,0 +1,7 @@
+// Test Craps
+public class CrapsTest {
+  public static void main(String[] args) {
+    Craps game = new Craps();
+    game.play();
+  }
+}
---
+++ 06_Methods/MethodOverloadTest.java
@@ -0,0 +1,7 @@
+// Test MethodOverload
+public class MethodOverloadTest {
+  public static void main(String[] args) {
+    MethodOverload mo = new MethodOverload();
+    mo.testOverloadedMethods();
+  }
+}
