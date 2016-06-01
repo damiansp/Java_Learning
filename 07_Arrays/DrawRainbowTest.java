1fc771ad1211f55b8dce50bbba2ba943488e860c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-06-01 18:33:33 -0400

    Example using colors, and an array of colors

---
+++ 07_Arrays/DrawRainbowTest.java
@@ -0,0 +1,14 @@
+// Test DrawRainbow
+import javax.swing.JFrame;
+
+public class DrawRainbowTest {
+  public static void main(String[] args) {
+    DrawRainbow panel = new DrawRainbow();
+    JFrame app = new JFrame();
+
+    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    app.add(panel);
+    app.setSize(400, 250);
+    app.setVisible(true);
+  }
+}
