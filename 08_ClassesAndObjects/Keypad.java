068426a519eaa967f1cc8c2e41317bd0fb877ae5
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-06-08 16:00:22 -0400

    Initial framework for an ATM program

---
+++ 08_ClassesAndObjects/Keypad.java
@@ -0,0 +1,12 @@
+// Represent an ATM keypad
+public class Keypad {
+  // Constructor
+  public Keypad() {
+    ; // TO DO...
+  }
+
+  public int getInput() {
+    ; // TO DO...
+  }
+    
+}
---
+++ 08_ClassesAndObjects/Withdrawal.java
@@ -0,0 +1,21 @@
+// Represent an ATM Withdrawal
+public class Withdrawal {
+  private int accountNumber;
+  private double amount;     // amount to withdraw
+
+  // refs to associated objs
+  private Screen screen; // ATM screen
+  private Keypad keypad; // ATM Keypad
+  private CashDispenser cashDispenser;  // ATM cash dispenser
+  private BankDatabase bankDB;          // account info DB
+    
+  // Constructor
+  public Withdrawal() {
+    ; // TO DO...
+  }
+
+  // Operations
+  public void execute() {
+    ; // TO DO...
+  }
+}
