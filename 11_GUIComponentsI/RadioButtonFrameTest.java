554388fa01627e17f59b45d8deff23d362c3b551
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-07-02 02:28:18 -0400

    GUI Radio buttons

---
+++ 11_GUIComponentsI/RadioButtonFrameTest.java
@@ -0,0 +1,12 @@
+// RadioButtonFrameTest.java
+// Test RadioButtonFrame class
+import javax.swing.JFrame;
+
+public class RadioButtonFrameTest {
+  public static void main(String[] args) {
+    RadioButtonFrame rbf = new RadioButtonFrame();
+    rbf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    rbf.setSize(300, 100);
+    rbf.setVisible(true);
+  }
+}
