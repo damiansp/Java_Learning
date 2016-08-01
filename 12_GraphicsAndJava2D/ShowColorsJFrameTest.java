b6f2d1ee58bdb3261463f4223156f1a8435617e8
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-08-01 19:27:23 -0400

    Color picker

---
+++ 12_GraphicsAndJava2D/ShowColorsJFrameTest.java
@@ -0,0 +1,10 @@
+/* ShowColorsJFrameTest.java */
+import javax.swing.JFrame;
+
+public class ShowColorsJFrameTest {
+  public static void main(String[] args) {
+    ShowColorsJFrame app = new ShowColorsJFrame();
+
+    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
