b599248cac0365763ab7e9ce44486c012a9a1c15
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-08-10 13:32:17 -0400

    Using assert

---
+++ 13_ExceptionHandling/Assert.java
@@ -0,0 +1,16 @@
+/* Assert.java */
+import java.util.Scanner;
+
+public class Assert {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a number between 0 and 10: ");
+    int number = input.nextInt();
+
+    // assert value is within range
+    assert (number >= 0 && number <= 10) : "bad number: " + number;
+    System.out.printf("You entered %d\n", number);
+  }
+
+}
