d1c3f8a05edb4b7d841678ac72060370ad21e08b
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-08-19 14:59:32 -0400

    Basic applet use and basic browser examples

---
+++ 24_Networking/ReadServerFileTest.java
@@ -0,0 +1,9 @@
+/* ReadServerFileTest.java */
+import javax.swing.JFrame;
+
+public class ReadServerFileTest {
+  public static void main(String[] args) {
+    ReadServerFile app = new ReadServerFile();
+    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+}
