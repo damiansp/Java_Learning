93c2cabe4f37d2892d7216bd338b482e920e1ae4
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-09-19 16:55:51 -0400

    GUI slider demo

---
+++ 22_GUIComponentsII/SliderFrame.java
@@ -0,0 +1,35 @@
+/* SliderFrame.java */
+// GUI slider demo
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class SliderFrame extends JFrame {
+  private JSlider diamSlider = new JSlider(
+    SwingConstants.HORIZONTAL, 0, 200, 10);
+  private OvalPanel panel = new OvalPanel();
+
+  // Constr
+  public SliderFrame() {
+    super("Slider Demo");
+    panel.setBackground(Color.BLACK);
+
+    // Set up slider
+    diamSlider.setMajorTickSpacing(10); // create a tick at every 10
+    diamSlider.setPaintTicks(true);
+
+    // Register listener
+    diamSlider.addChangeListener(new ChangeListener() {
+        public void stateChanged(ChangeEvent e) {
+          panel.setDiameter(diamSlider.getValue());
+        }
+    });
+
+    add(diamSlider, BorderLayout.SOUTH);
+    add(panel, BorderLayout.CENTER);
+  }
+}
