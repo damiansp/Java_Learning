83136775bded1f161888494287b2134463cac69d
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-09-20 19:35:09 -0400

    Menus in GUIs

---
+++ 22_GUIComponentsII/MenuTest.java
@@ -0,0 +1,11 @@
+/* MenuTest.java */
+import javax.swing.JFrame;
+
+public class MenuTest {
+  public static void main(String[] args) {
+    MenuFrame mf = new MenuFrame();
+    mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mf.setSize(500, 200);
+    mf.setVisible(true);
+  }
+}
