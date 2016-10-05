72b7125e61d0fbd598739e7cb1d8f93ba9c81cb6
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-10-05 16:06:12 -0400

    Look and Feel built-ins demo

---
+++ 22_GUIComponentsII/LookAndFeelTest.java
@@ -0,0 +1,11 @@
+/* LookAndFeelTest.java */
+import javax.swing.JFrame;
+
+public class LookAndFeelTest {
+  public static void main(String[] args) {
+    LookAndFeelFrame laff = new LookAndFeelFrame();
+    laff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    laff.setSize(400, 220);
+    laff.setVisible(true);
+  }
+}
