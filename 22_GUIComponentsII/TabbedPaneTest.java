4a72668b911aca340e77960ac0df8d9ee573e4c5
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-10-07 19:17:20 -0400

    JTabbedPane demo

---
+++ 22_GUIComponentsII/TabbedPaneTest.java
@@ -0,0 +1,11 @@
+/* TabbedPaneTest.java */
+import javax.swing.JFrame;
+
+public class TabbedPaneTest {
+  public static void main(String[] args) {
+    TabbedPane tabby = new TabbedPane();
+    tabby.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    tabby.setSize(300, 200);
+    tabby.setVisible(true);
+  }
+}
