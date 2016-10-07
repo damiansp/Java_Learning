fefab8d1f65b52a732241d72ef405110511f8f23
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-10-07 19:39:07 -0400

    BoxLayout demo

---
+++ 22_GUIComponentsII/BoxLayoutTest.java
@@ -0,0 +1,11 @@
+/* BoxLayoutTest.java */
+import javax.swing.JFrame;
+
+public class BoxLayoutTest {
+  public static void main(String[] args) {
+    BoxLayoutFrame blf = new BoxLayoutFrame();
+    blf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    blf.setSize(400, 250);
+    blf.setVisible(true);
+  }
+}
