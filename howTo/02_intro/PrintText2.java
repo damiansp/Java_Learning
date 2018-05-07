4660ed12b71a872700073b0a9f9ed024f911c8ea
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-06 21:18:33 -0400

    printing output examples

---
+++ howTo/02_intro/PrintText2.java
@@ -0,0 +1,8 @@
+// PrintText2.java
+
+public class PrintText2 {
+  public static void main(String[] args) {
+    System.out.print("Welcome to");
+    System.out.println(" your life!");
+  }
+}
---
+++ howTo/02_intro/PrintText3.java
@@ -0,0 +1,7 @@
+// PrintText3.java
+
+public class PrintText3 {
+  public static void main(String[] args) {
+    System.out.println("Welcome\nto\nYour Life!");
+  }
+}
