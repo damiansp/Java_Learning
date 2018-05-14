597fb7eda85efdb8766f91c61a5e651f24a74e3e
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-14 15:00:14 -0700

    adds Account and test

---
+++ howTo/03_classesObjectsMethdodsStrings/Account.java
@@ -0,0 +1,13 @@
+// Account.java
+
+public class Account {
+  private String name;
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ howTo/03_classesObjectsMethdodsStrings/AccountTest.java
@@ -0,0 +1,16 @@
+// AccountTest.java
+import java.util.Scanner;
+
+public class AccountTest {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Account myAccount = new Account();
+
+    System.out.printf("Initial name is: %s\n\n", myAccount.getName());
+    System.out.println("Please enter the name:");
+    String name = input.nextLine();
+
+    myAccount.setName(name);
+    System.out.printf("\nName in myAccount is:\n%s\n", myAccount.getName());
+  }
+}
