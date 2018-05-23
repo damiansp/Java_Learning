fd22ff3f0833d2927d289143a6cea470b3c0e428
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-23 08:16:13 -0700

    adds methods to Account

--- howTo/03_classesObjectsMethdodsStrings/Account.java
+++ howTo/03_classesObjectsMethdodsStrings/Account.java
@@ -2,6 +2,28 @@
 
 public class Account {
   private String name;
+  private double balance;
+
+  public Account(String name, double balance) {
+    this.name = name;
+    if (balance > 0.0) {
+      this.balance = balance;
+    } else {
+      this.balance = 0.0;
+    }
+  }
+
+  public void deposit(double amount) {
+    if (amount > 0.0) {
+      balance += amount;
+    } else {
+      System.out.println("Deposit amount must be greater than 0.");
+    }
+  }
+
+  public double getBalance() {
+    return balance;
+  }
 
   public void setName(String name) {
     this.name = name;
