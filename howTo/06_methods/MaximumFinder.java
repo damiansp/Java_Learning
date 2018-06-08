23e36a7d358d8c9823efc749459ec2e0de2eba41
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-08 14:50:26 -0700

    adds max finder

--- howTo/06_methods/MaximumFinder.java
+++ howTo/06_methods/MaximumFinder.java
@@ -1,1 +1,29 @@
 // MaximumFinder.java
+import java.util.Scanner;
+
+public class MaximumFinder {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter 3 decimal values separated by spaces: ");
+    double n1 = input.nextDouble();
+    double n2 = input.nextDouble();
+    double n3 = input.nextDouble();
+    double result = maximum(n1, n2, n3);
+
+    System.out.println("Max is: " + result);
+  }
+
+
+  public static double maximum(double x, double y, double z) {
+    double maxValue = x;
+
+    if (y > maxValue) {
+      maxValue = y;
+    }
+    if (z > maxValue) {
+      maxValue = z;
+    }
+    return maxValue;
+  }
+}
