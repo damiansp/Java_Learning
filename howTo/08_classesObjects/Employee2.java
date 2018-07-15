7e60103a4e2857ca4878c0147dcab75beb75f3b8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-15 18:39:19 -0400

    ++Employee2 + Test

---
+++ howTo/08_classesObjects/Employee2.java
@@ -0,0 +1,19 @@
+public class Employee2 {
+  private static int count = 0; // static -> class var, not instance
+  private String firstName;
+  private String lastName;
+
+  public Employee2(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    ++count;
+    System.out.printf(
+      "Employee constructor: %s %s; count = %d\n", firstName, lastName, count);
+  }
+
+  public String getFirstName() { return firstName; }
+
+  public String getLastName() { return lastName; }
+
+  public static int getCount() { return count; }
+}
