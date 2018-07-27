2b501b03512a2689512074b27c1914deabdf9698
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-27 08:21:39 -0700

    08++

---
+++ howTo/08_classesObjects/PackageDataTest.java
@@ -0,0 +1,20 @@
+public class PackageDataTest {
+  public static void main(String[] args) {
+    PackageData packageData = new PackageData();
+
+    System.out.printf("After instantiation:\n%s\n", packageData);
+    packageData.number = 77;
+    packageData.string = "Goodbye";
+    System.out.printf("After changing value:\n%s\n", packageData);
+  }
+}
+
+
+class PackageData {
+  int number = 0;
+  String string = "Hello";
+
+  public String toString() {
+    return String.format("number: %d; string: %s", number, string);
+  }
+}
