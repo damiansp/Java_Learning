040779f77c471c941952ef2ec01ce28c634b566a
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-04-08 11:18:54 -0700

    add PrintTask and Executor

---
+++ howTo/23_concurrency/PrintTask.java
@@ -0,0 +1,28 @@
+import java.security.SecureRandom;
+
+
+public class PrintTask implements Runnable {
+  private static final SecureRandom rand = new SecureRandom();
+  private final int sleepTime;
+  private final String taskName;
+
+  
+  public PrintTask(String taskName) {
+    this.taskName = taskName;
+    sleepTime = rand.nextInt(5000);
+  }
+
+
+  @Override
+  public void run() {
+    try {
+      System.out.printf(
+        "%s going to sleep for %d milliseconds.%n", taskName, sleepTime);
+      Thread.sleep(sleepTime);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      Thread.currentThread().interrupt();
+    }
+    System.out.printf("%s done sleeping\n", taskName);
+  }
+}
