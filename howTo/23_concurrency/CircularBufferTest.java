91ff2fa79a2b511545f1d7c50b605e338f229448
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-11-19 07:14:17 -0800

    added all circular buffer test

---
+++ howTo/23_concurrency/CircularBufferTest.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class CircularBufferTest {
+  public static void main(String[] args) throws InterruptedException {
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    CircularBuffer sharedLocation = new CircularBuffer();
+
+    sharedLocation.displayState("Initial State");
+    executorService.execute(new Producer(sharedLocation));
+    executorService.execute(new Consumer(sharedLocation));
+    executorService.shutdown();
+    executorService.awaitTermination(1, TimeUnit.MINUTES);
+  }
+}
+  
