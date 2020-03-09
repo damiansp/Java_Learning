2a5a4f18630c7b15d54818e48f51021e29b05f3b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-03-09 08:46:28 -0700

    init BigInt

---
+++ core/v1/03_FundamentalStructures/BigIntTest.java
@@ -0,0 +1,10 @@
+import java.math.*;
+import java.util.*;
+
+/**
+ * Program using BigInteger to compute lottery odds
+ * Derived from code by Cay Horstmann (see README)
+ */
+public class BigIntTest {
+
+}
